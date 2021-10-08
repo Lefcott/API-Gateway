9cf16e614e8cfe47cceb77929d8590cb3876d49e
Leandro Cotti <lefcott@hotmail.com>
2021-10-08 15:17:14 -0300

    add origin

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json({ extended: false }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://blanqueriawow.com');
 
   next();
 });
