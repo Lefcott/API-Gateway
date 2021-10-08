deb93678437db27ea42b83e2337fd175517157f4
Leandro Cotti <lefcott@hotmail.com>
2021-10-08 14:40:11 -0300

    test

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const product = require("./api/product");
+const product = require('./api/product');
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/product", product);
+app.use('/api/product', product);
+
+app.get('/test', (req, res) => {
+  res.json({
+    message: 'welcome',
+  });
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
